6cb6e3995ebd79bace5db276d5dd062e446d17fa
James <jtmacon36@gmail.com>
2019-10-25 11:06:07 -0400

    adding trivia cpt and google maps key

--- theme/classes/theme.php
+++ theme/classes/theme.php
@@ -4,6 +4,8 @@ use Trivia\CustomPostType as CPT;
 use Trivia\CustomTaxonomy as CTX;
 use Trivia\CustomEndpoint as CEP;
 
+require_once __DIR__ . '/../keys.php';
+
 class TriviaAdminTheme
 {
   function __construct() {
@@ -11,11 +13,25 @@ class TriviaAdminTheme
     add_action('init', [$this, 'custom_post_types']);
     add_action('init', [$this, 'custom_taxonomies']);
     add_action('rest_api_init', [$this, 'custom_endpoints']);
+    add_action('acf/init', [$this, 'acf_google_maps_api_key']);
+    // Filters
+    add_filter('acf/fields/google_map/api', [$this, 'acf_google_map']);
   }
 
   function theme_supports() {
     add_theme_support('html5');
     add_theme_support('menus');
+  }
+
+  function acf_google_map($api) {
+    global $google_maps_key;
+    $api['key'] = $google_maps_key;
+    return $api;
+  }
+
+  function acf_google_maps_api_key() {
+    global $google_maps_key;
+    acf_update_setting('google_api_key', $google_maps_key);
   }
   
   function get_populated_terms($term) {
@@ -43,7 +59,7 @@ class TriviaAdminTheme
   }
 
   function custom_post_types() {
-    // new CPT('Distributor', 'Distributors', 'dashicons-admin-site-alt3', 20);
+    new CPT('Trivia Event', 'Trivia Events', 'dashicons-tickets-alt', 20);
   }
 
   function custom_taxonomies() {
